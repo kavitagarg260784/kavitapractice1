0f234b2845df7a115954a10139827a43b6775e16
Gajendra <Gajendra@DESKTOP-H81M1QG>
2023-03-21 14:37:02 +0530

    Add project files.

---
+++ Models/IPieReposatory.cs
@@ -0,0 +1,12 @@
+﻿namespace kavitapractice1.Models
+{
+    public interface IPieReposatory
+    {
+
+
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+        IEnumerable<Pie> SearchPies(string searchQuery);
+    }
+}
